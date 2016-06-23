1dd7421f7e7e2d2df6c82fa2169bbdfe50a6d94e
Luiz Felipe Weber <luizwbr@gmail.com>
2016-06-23 18:01:55 -0300

    Correção serviços sem contrato
    Correção para os serviços sem contrato, para que não seja enviado o usuário e senha.

--- correios_virtuemartbrasil.php
+++ correios_virtuemartbrasil.php
@@ -434,8 +434,12 @@ class plgVmShipmentCorreios_Virtuemartbrasil extends vmPSPlugin {
             $this->Order_Height = "2";
         }
 
-        $workstring = 'nCdEmpresa='.trim($method->Usuario_SN);
-        $workstring.='&sDsSenha='.trim($method->Senha_SN);
+        $servicos_com_contrato = array("81019","81027","81035","81868","81833","81850","40126","40436","40444","40568","40606","41068","41300");
+        $workstring = "";
+        if (in_array($method->Servicos_SN,$servicos_com_contrato)) {
+            $workstring = 'nCdEmpresa='.trim($method->Usuario_SN);
+            $workstring.='&sDsSenha='.trim($method->Senha_SN);    
+        } 
         $workstring.='&nVlDiametro=0';
         $workstring.='&sCdMaoPropria=0';
         $workstring.='&nCdServico='. $method->Servicos_SN;
